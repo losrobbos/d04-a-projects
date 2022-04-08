fec356c3bbb0b02188dcef93d8c247469d09a5ed
losrobbos <losrobbos@web.de>
2022-04-08 12:23:03 +0200

    Polishing

--- react-hello-world/src/App.js
+++ react-hello-world/src/App.js
@@ -1,17 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
 function App() {
 
-  // STATE ARRAYS
+  // STATE ARRAYS => React observes this thing here
   const [todos, setTodos] = useState([
     "Wake the cat", "Go out with the dog", "Clean the dishes", "New one"
-  ]) // => React observes this thing here
+  ]) 
 
   const addTodo = () => {
     console.log("AddTodo called...")
+    
+    // THIS line triggers a state update 
+    // => and React will re-render the HTML below (in the return)!
     setTodos( [ ...todos, "The next one" ] )
+    
     // todos.push( "The next one" ) // this does not work
     console.log( todos )
   } 
@@ -22,6 +25,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h2>Hallo dudes</h2>
+        {/* Javascript in HTML - crazy stuff... jaja.. */}
         { todos.map( todo => <div>{todo}</div> ) }
         <button onClick={ addTodo } >Add Todo</button>
       </header>
